499e168f1fa207c9f3edaaadaa6e6609018b21ba
MEHDI ZAMITTI <121120626+mehdizamm@users.noreply.github.com>
2023-05-31 23:31:11 +0200

    save03

--- quete01/src/App.jsx
+++ quete01/src/App.jsx
@@ -24,16 +24,62 @@ function App() {
 
     {
 
+      name: "charmander",
+
+      imgSrc:
+
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+
+    },
+
+    {
+
+      name: "squirtle",
+
+      imgSrc:
+
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+
+    },
+
+    {
+
+      name: "pikachu",
+
+      imgSrc:
+
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+
+    },
+
+    {
+
       name: "mew",
 
     },
 
   ];
 
-  return (
+  const [currentIndex, setCurrentIndex] = useState(0);
 
+
+  function handlePrevious() {
+    setCurrentIndex(function (prevIndex) {
+      return prevIndex === 0 ? pokemonList.length - 1 : prevIndex - 1;
+    });
+  }
+
+  function handleNext() {
+    setCurrentIndex(function (prevIndex) {
+      return prevIndex === pokemonList.length - 1 ? 0 : prevIndex + 1;
+    });
+  }
+
+  return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]}></PokemonCard>
+      <PokemonCard pokemon={pokemonList[currentIndex]} />
+      <button onClick={handlePrevious}>Previous</button>
+      <button onClick={handleNext}>Next</button>
     </div>
   );
 }
